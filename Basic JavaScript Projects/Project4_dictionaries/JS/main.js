c67b57ee3cc12f7c5b69d11dd5bd2e03e87760c3
Andy <andrewrobertblack@gmail.com>
2021-01-27 07:20:06 -0800

    fix java project 4

--- Basic JavaScript Projects/Project4_dictionaries/JS/main.js
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -1,10 +1,10 @@
 function myDictionary() {
-    var Cat = {
+    var Cat = { //defining Key Value Pairs
         Hair:"Long",
         Color:"Orange",
         Age:"8",
     };
-    delete Cat.Hair;
+    delete Cat.Hair; //deleting kvp 
     document.getElementById("dictionary").innerHTML = Cat.Hair;
-}
+} //output is undefinded because we deleted the kvp before it displayed
 
